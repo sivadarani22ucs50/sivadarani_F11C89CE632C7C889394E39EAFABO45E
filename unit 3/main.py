78be5d7b3683f481169f0015b8bc4b42d7702636
sivadarani22ucs50 <145139533+sivadarani22ucs50@users.noreply.github.com>
2023-10-09 17:38:17 +0100

    Add files via upload

---
+++ unit 3/main.py
@@ -0,0 +1,16 @@
+#3.1
+def 
+linearSearhProduct(productList, targetProduct):
+  indices = []
+  for index, product in enumerate(productList):
+    indices.append(index)
+
+return indices
+
+
+#Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target-"shoes"
+target2='apple'
+result=linearSearchProduct(product, target)
+print(result)
